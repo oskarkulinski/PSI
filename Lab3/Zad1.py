b9a5ee84cbc0cba1273b713765d882c8c30d684f
Oskar Kulinski <oskar.kulinski@gmail.com>
2024-03-11 13:29:24 +0100

    11.03 po zajeciach

---
+++ Lab3/Zad1.py
@@ -0,0 +1,24 @@
+import numpy as np
+from scipy import stats
+import pandas as pd
+import matplotlib.pyplot as plt
+
+f = lambda x: (x)
+x_tr = np.linspace(0., 5, 200)
+y_tr = f(x_tr)
+x = stats.uniform(1,3).rvs(100)
+y = f(x) + stats.norm(0,0.1).rvs(len(x))
+
+plt.figure(figsize=(6,6));
+axes = plt.gca()
+axes.set_xlim([0,5])
+axes.set_ylim([0,5])
+plt.plot(x_tr, y_tr, '--k');
+plt.plot(x, y, 'ok', ms=3);
+plt.show()
+
+corr = {}
+corr['pearson'], _ = stats.pearsonr(x,y)
+corr['spearman'], _ = stats.spearmanr(x,y)
+corr['kendall'], _ = stats.kendalltau(x,y)
+print(corr)
---
+++ Lab3/Zad5.py
@@ -0,0 +1,25 @@
+import numpy as np
+from scipy import stats
+import pandas as pd
+import matplotlib.pyplot as plt
+
+f = lambda x: 2 * np.sin(x)
+x_tr = np.linspace(-3, 3, 200)
+y_tr = f(x_tr)
+x = stats.uniform(-1,5).rvs(100)
+y = f(x) + stats.norm(0,0.3).rvs(len(x))
+
+plt.figure(figsize=(6,6));
+axes = plt.gca()
+axes.set_xlim([-3,3])
+axes.set_ylim([-5,5])
+# axes.set_ylim([np.min(y),f(5)])
+plt.plot(x_tr, y_tr, '--k');
+plt.plot(x, y, 'ok', ms=3);
+
+plt.show()
+corr = {}
+corr['pearson'], _ = stats.pearsonr(x,y)
+corr['spearman'], _ = stats.spearmanr(x,y)
+corr['kendall'], _ = stats.kendalltau(x,y)
+print(corr)
